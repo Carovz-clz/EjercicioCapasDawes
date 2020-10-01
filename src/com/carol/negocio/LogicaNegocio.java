19a2706a613fb26460936fd4c6b02df08a4e8551
carolZapata <carozapatac@gmail.com>
2020-10-01 09:45:33 +0200

    [ADD] Capa de lógica de negocio

---
+++ src/com/carol/negocio/LogicaNegocio.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.carol.negocio;
+
+/**
+ * @author carol
+ *
+ */
+public class LogicaNegocio {
+	
+	public  Integer cuantosCoches(String nombre) {
+		//Usar clase datos
+		ConsultaDatos consultaDatos = 
+		return null;
+	}
+
+}
